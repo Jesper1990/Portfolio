f8f6ebfd2bb50d7b4255c57fc0e9e282db7e77cc
Jesper Lundbäck <68069660+Jesper1990@users.noreply.github.com>
2021-06-16 13:30:51 +0200

    started working on SPA. Building up the site

---
+++ js/dynamicPages.js
@@ -0,0 +1,6 @@
+const dynamicPages = {
+  '#error': () => `<div class="main-content">Oops, something went wrong!</div>`,
+  '#projects': () => () => $('main').load('/html-partials/projects.html'),
+  '#about': () => () => $('main').load('/html-partials/about.html'),
+  '#contact': () => () => $('main').load('/html-partials/contact.html')
+}
---
+++ js/main.js
@@ -0,0 +1,12 @@
+async function loadMainContent() {
+
+  let dynamicPage = window.location.hash.split("?")[0];
+  page = await dynamicPages[dynamicPage]();
+  console.log(dynamicPage)
+  if (!page) {
+    page = dynamicPages["error"]()
+  }
+  $("main").html(page)
+}
+window.onhashchange = loadMainContent;
+setTimeout(loadMainContent, 0);
