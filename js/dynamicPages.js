fcb4783b6346cf5e174f73689e2fbb55f500a94c
Jesper Lundbäck <68069660+Jesper1990@users.noreply.github.com>
2021-06-17 12:50:25 +0200

    Fixed a countdown timer, not complete

--- js/dynamicPages.js
+++ js/dynamicPages.js
@@ -2,5 +2,6 @@ const dynamicPages = {
   '#error': () => `<div class="main-content">Oops, something went wrong!</div>`,
   '#projects': () => () => $('main').load('/html-partials/projects.html'),
   '#about': () => () => $('main').load('/html-partials/about.html'),
-  '#contact': () => () => $('main').load('/html-partials/contact.html')
+  '#contact': () => () => $('main').load('/html-partials/contact.html'),
+  '#random-timer': () => () => $('main').load('/html-partials/random-timer.html')
 }
